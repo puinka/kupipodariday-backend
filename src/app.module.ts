ecd2349263fcc475d0276d784c87d84bab37a643
Valeria <vozzheva@mail.ru>
2023-05-27 09:38:29 +0300

    ref: move configuration to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+import { AppDataSource } from './ormconfig';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'kupipodariday',
-      entities: [],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync(AppDataSource),
     UsersModule,
     WishesModule,
     WishlistsModule,
