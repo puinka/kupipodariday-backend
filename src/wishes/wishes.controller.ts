350dd40e4627abcf4b6da736e73d054c94e1fbad
Valeria <vozzheva@mail.ru>
2023-05-28 15:50:08 +0300

    feat: wishes methods without auth

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { WishesService } from './wishes.service';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
@@ -7,28 +16,51 @@ import { UpdateWishDto } from './dto/update-wish.dto';
 export class WishesController {
   constructor(private readonly wishesService: WishesService) {}
 
-  @Post()
-  create(@Body() createWishDto: CreateWishDto) {
-    return this.wishesService.create(createWishDto);
+  // POST/wishes
+
+  // GET/wishes/last
+  @Get('last')
+  async findLastWishes() {
+    const wishes = await this.wishesService.findLastWishes();
+    if (!wishes) throw new NotFoundException(`No wishes found`);
+    return wishes;
   }
 
-  @Get()
-  findAll() {
-    return this.wishesService.findAll();
+  // GET/wishes/top
+  @Get('top')
+  async findTopWishes() {
+    const wishes = await this.wishesService.findTopWishes();
+    if (!wishes) throw new NotFoundException(`No wishes found`);
+    return wishes;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.wishesService.findOne(+id);
-  }
+  // GET/wishes/{id}
+  // PATCH/wishes/{id}
+  // DELETE/wishes/{id}
+  // POST/wishes/{id}/copy
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
-    return this.wishesService.update(+id, updateWishDto);
-  }
+  // @Post()
+  // create(@Body() createWishDto: CreateWishDto) {
+  //   return this.wishesService.create(createWishDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wishesService.remove(+id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.wishesService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.wishesService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateWishDto: UpdateWishDto) {
+  //   return this.wishesService.update(+id, updateWishDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.wishesService.remove(+id);
+  // }
 }
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { CreateWishDto } from './dto/create-wish.dto';
 import { UpdateWishDto } from './dto/update-wish.dto';
+import { Wish } from './entities/wish.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class WishesService {
-  create(createWishDto: CreateWishDto) {
-    return 'This action adds a new wish';
+  constructor(
+    @InjectRepository(Wish)
+    private readonly wishRepository: Repository<Wish>,
+  ) {}
+
+  async findLastWishes(): Promise<Wish[]> {
+    return await this.wishRepository.find({
+      take: 40,
+      order: { createdAt: 'DESC' },
+    });
   }
 
-  findAll() {
-    return `This action returns all wishes`;
+  async findTopWishes(): Promise<Wish[]> {
+    return await this.wishRepository.find({
+      take: 10,
+      order: { copied: 'DESC' },
+    });
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} wish`;
-  }
-
-  update(id: number, updateWishDto: UpdateWishDto) {
-    return `This action updates a #${id} wish`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} wish`;
-  }
+  // create(createWishDto: CreateWishDto) {
+  //   return 'This action adds a new wish';
+  // }
+  // findAll() {
+  //   return `This action returns all wishes`;
+  // }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} wish`;
+  // }
+  // update(id: number, updateWishDto: UpdateWishDto) {
+  //   return `This action updates a #${id} wish`;
+  // }
+  // remove(id: number) {
+  //   return `This action removes a #${id} wish`;
+  // }
 }
