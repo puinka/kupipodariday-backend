0afe5a3c386732655609235a7b8b2271c9f6bfad
Valeria <vozzheva@mail.ru>
2023-05-28 15:52:46 +0300

    ref: clean offers methods without auth

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { UpdateOfferDto } from './dto/update-offer.dto';
@@ -7,28 +15,32 @@ import { UpdateOfferDto } from './dto/update-offer.dto';
 export class OffersController {
   constructor(private readonly offersService: OffersService) {}
 
-  @Post()
-  create(@Body() createOfferDto: CreateOfferDto) {
-    return this.offersService.create(createOfferDto);
-  }
+  // POST/offers
+  // GET/offers
+  // GET/offers/{id}
 
-  @Get()
-  findAll() {
-    return this.offersService.findAll();
-  }
+  // @Post()
+  // create(@Body() createOfferDto: CreateOfferDto) {
+  //   return this.offersService.create(createOfferDto);
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.offersService.findOne(+id);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.offersService.findAll();
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
-    return this.offersService.update(+id, updateOfferDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.offersService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.offersService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateOfferDto: UpdateOfferDto) {
+  //   return this.offersService.update(+id, updateOfferDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.offersService.remove(+id);
+  // }
 }
