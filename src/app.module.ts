d068e1da12473fa15394625e4573b64e78b14e23
Valeria <vozzheva@mail.ru>
2023-05-28 15:40:06 +0300

    fix: removed .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,23 @@ import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
-import { AppDataSource } from './ormconfig';
+import { User } from './users/entities/user.entity';
+import { Offer } from './offers/entities/offer.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      entities: [User, Offer, Wish, Wishlist],
+      synchronize: false,
     }),
-    TypeOrmModule.forRootAsync(AppDataSource),
     UsersModule,
     WishesModule,
     WishlistsModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
