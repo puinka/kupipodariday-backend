49774d1b9bd3e0c6300eb6143b4ddb6178632504
Valeria <vozzheva@mail.ru>
2023-05-28 14:53:33 +0300

    feat: user controller methods from swagger without auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,57 +3,87 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
-  ParseIntPipe,
   NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { Wish } from 'src/wishes/entities/wish.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post('signup')
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
-    const newUser = await this.usersService.create(createUserDto);
-    return newUser;
+  // GET/users/me
+  // PATCH/users/me
+  // GET/users/me/wishes
+
+  // GET/users/{username}
+  @Get(':username')
+  async findUserByUsername(@Param('username') username: string): Promise<User> {
+    const user = await this.usersService.findUserByUsername(username);
+    if (!user) throw new NotFoundException(`User not found`);
+    return user;
   }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
+  // GET/users/{username}/wishes
+  @Get(':username/wishes')
+  async findWishesByUsername(
+    @Param('username') username: string,
+  ): Promise<Wish[]> {
+    const { id } = await this.usersService.findUserByUsername(username);
+    if (!id) throw new NotFoundException(`User not found`);
+
+    const wishes = await this.usersService.findUserWishes(id);
+    if (!wishes) throw new NotFoundException(`User has no wishes`);
+
+    return wishes;
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.usersService.findById(id);
+  // POST/users/find
+  @Post('find')
+  async findByEmail(@Body() query: string): Promise<User[]> {
+    const user = await this.usersService.findByEmail(query);
+    if (!user) throw new NotFoundException(`User not found`);
+    return user;
   }
 
-  @Patch(':id')
-  async updateById(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateUserDto: UpdateUserDto,
-  ) {
-    const user = await this.usersService.findById(id);
-    if (!user) {
-      throw new NotFoundException(`User not found`);
-    }
+  // @Post('signup')
+  // async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+  //   return await this.usersService.create(createUserDto);
+  // }
 
-    return this.usersService.updateById(id, updateUserDto);
-  }
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
 
-  @Delete(':id')
-  async removeById(@Param('id', ParseIntPipe) id: number) {
-    const user = await this.usersService.findById(id);
-    if (!user) {
-      throw new NotFoundException(`User not found`);
-    }
+  // @Get(':id')
+  // findOne(@Param('id', ParseIntPipe) id: number) {
+  //   return this.usersService.findById(id);
+  // }
+  //@Patch(':id')
+  // async updateById(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Body() updateUserDto: UpdateUserDto,
+  // ) {
+  //   const user = await this.usersService.findById(id);
+  //   if (!user) {
+  //     throw new NotFoundException(`User not found`);
+  //   }
 
-    return this.usersService.removeById(id);
-  }
+  //   return this.usersService.updateById(id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // async removeById(@Param('id', ParseIntPipe) id: number) {
+  //   const user = await this.usersService.findById(id);
+  //   if (!user) {
+  //     throw new NotFoundException(`User not found`);
+  //   }
+
+  //   return this.usersService.removeById(id);
+  // }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,35 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Wish } from 'src/wishes/entities/wish.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(Wish)
+    private readonly wishRepository: Repository<Wish>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    const newUser = this.userRepository.create(createUserDto);
-    return await this.userRepository.save(newUser);
+  // async create(createUserDto: CreateUserDto): Promise<User> {
+  //   const newUser = this.userRepository.create(createUserDto);
+  //   return await this.userRepository.save(newUser);
+  // }
+
+  // findAll(): Promise<User[]> {
+  //   return this.userRepository.find();
+  // }
+
+  async findUserByUsername(username: string): Promise<User> {
+    return await this.userRepository.findOneBy({ username });
   }
 
-  findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async findUserWishes(id: number): Promise<Wish[]> {
+    const wishes = await this.wishRepository.find({
+      where: { owner: { id } },
+    });
+    return wishes;
   }
 
-  findById(id: number): Promise<User> {
-    return this.userRepository.findOneBy({ id });
+  async findByEmail(query: string): Promise<User[]> {
+    const user = await this.userRepository.find({
+      where: [{ email: query }],
+    });
+    return user;
   }
 
-  updateById(id: number, updateUserDto: UpdateUserDto) {
-    return this.userRepository.update({ id }, updateUserDto);
-  }
+  // updateById(id: number, updateUserDto: UpdateUserDto) {
+  //   return this.userRepository.update({ id }, updateUserDto);
+  // }
 
-  removeById(id: number) {
-    return this.userRepository.delete({ id });
-  }
+  // removeById(id: number) {
+  //   return this.userRepository.delete({ id });
+  // }
 }
