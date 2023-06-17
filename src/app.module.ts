2f0c5d9950db0d871e85f2b823e94dcbd7e7de9a
Valeria <vozzheva@mail.ru>
2023-06-17 09:41:29 +0300

    ref: configuration from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,18 @@ import { Offer } from './offers/entities/offer.entity';
 import { Wish } from './wishes/entities/wish.entity';
 import { Wishlist } from './wishlists/entities/wishlist.entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'kupipodariday',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'student',
+      password: process.env.DB_PASS || 'student',
+      database: process.env.DB_NAME || 'kupipodariday',
       entities: [User, Offer, Wish, Wishlist],
       synchronize: false,
     }),
